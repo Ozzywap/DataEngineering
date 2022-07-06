aa159d8b43b9e0139ef9e506f5a9540c98a00568
Osman Haji <osman6191@outlook.com>
2022-07-07 08:17:49 +1000

    bash script

--- Projects/First_project/src/sourceTextFiles.sh
+++ Projects/First_project/src/sourceTextFiles.sh
@@ -8,29 +8,4 @@ var=`ls -l sample-text-file.txt | awk '{print $5}'`
 
 #store line count
 var=`wc sample-text-file.txt | awk '{print $1}'`
-
-BULK INSERT dbo.demo
-  FROM '/Users/osmanhaji/Desktop/sample-text-file.txt'
-  WITH  
-     (
-        ROWTERMINATOR ='\n'
-     );
-
-select * from demo
-
-/Users/osmanhaji/Desktop/sample.txt
-
-
--- truncate the table first
-TRUNCATE TABLE demo;
-GO
- 
-
-BULK INSERT demo.Actors
-FROM '/Users/osmanhaji/Desktop/actor.csv'
-WITH
-(
-        FORMAT='CSV',
-        FIRSTROW=2
-)
-GO
+`
