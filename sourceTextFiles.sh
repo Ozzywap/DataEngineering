1cd16f7fe5e4265289f4f7c8f43a172b7cb3fe75
Osman Haji <osman6191@outlook.com>
2022-07-07 08:13:03 +1000

    restructure

--- sourceTextFiles.sh
+++
@@ -1,8 +0,0 @@
-#download file to current directory using curl
-curl -O https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt
-
-#store size of file in bytes
-var=`wc sample-text-file.txt | awk '{print $3}'`
-
-#store line count
-var=`wc sample-text-file.txt | awk '{print $1}'`
